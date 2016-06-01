e790dadddfc78eb71c873cdc967718869435836e
Oliver Evans <oliverevans96@gmail.com>
2016-06-01 10:17:31 -0400

    Rename data files, create python script to parse - not finished

---
+++ python/light_data.py
@@ -0,0 +1,10 @@
+# light_data.py
+# Oliver Evans
+# Clarkson University REU 2016
+# Created: 01-06-16 10:11AM
+# Last Edited: 01-06-16 10:12AM
+
+from numpy import *
+from matplotlib.pyplot import *
+from pandas import *
+
