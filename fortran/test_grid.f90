77c25abea754dcb5aa888f36f8e71f23d94b8541
Oliver Evans <oliverevans96@gmail.com>
2018-03-23 03:41:41 -0400

    Add p conversion test

--- fortran/test_grid.f90
+++ fortran/test_grid.f90
@@ -5,6 +5,10 @@ program test_grid
 
   call test_2d_angular_integration(f_ang, 10, 10, 13.823007d0)
   call test_2d_angular_integration(f_ang, 20, 20, 12.570662d0)
+
+  call test_angle_p_conversions(10, 10)
+  call test_angle_p_conversions(15, 25)
+  call test_angle_p_conversions(30, 15)
 end program test_grid
 
 function f_ang(theta, phi)
--- fortran/test_grid_mod.f90
+++ fortran/test_grid_mod.f90
@@ -30,4 +30,45 @@ contains
     call grid%deinit()
   end subroutine test_2d_angular_integration
 
+  subroutine test_angle_p_conversions(ntheta, nphi)
+  integer ntheta, nphi
+  logical passed
+  integer l, m, p
+  type(space_angle_grid) grid
+
+  call grid%set_bounds(-1.d0,1.d0,-2.d0,2.d0,-3.d0,3.d0)
+  call grid%set_num(4,4,4, ntheta, nphi)
+  call grid%init()
+
+  passed = .true.
+
+  ! Make sure that conversions between l,m and p
+  ! are invertible on interior of angular grid
+  do l=1, ntheta
+     do m=2, nphi-1
+        p = grid%angles%phat(l, m)
+        if(grid%angles%lhat(p) .ne. l) then
+           passed = .false.
+           exit
+        end if
+        if(grid%angles%mhat(p) .ne. m) then
+           passed = .false.
+           exit
+        end if
+     end do
+     if(.not. passed) then
+        exit
+     end if
+  end do
+
+  if(passed) then
+     write(*,'(A,I2,A,I2)') '* PASS - test_p_conversions: ', ntheta, 'x', nphi
+  else
+     write(*,'(A,I2,A,I2)') '* FAIL - test_p_conversions: ', ntheta, 'x', nphi
+  end if
+
+  call grid%deinit()
+
+  end subroutine test_angle_p_conversions
+
 end module
