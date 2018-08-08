2682844c66785a8429fd66360a4981f5c1e1abf9
Oliver Evans <oliverevans96@gmail.com>
2018-08-08 11:54:36 -0400

    Global and pointwise options in uneven_diff.py

--- code/python/uneven_diff.py
+++ code/python/uneven_diff.py
@@ -46,15 +46,15 @@ def plot_discrete_diff(xmin, xmax, y1, y2):
 
     n1 = len(y1)
     n2 = len(y2)
-    
+
     e1 = np.linspace(xmin, xmax, n1+1)
     e2 = np.linspace(xmin, xmax, n2+1)
-    
+
     x1 = e1[:-1] + np.diff(e1)/2
     x2 = e2[:-1] + np.diff(e2)/2
 
     e3, z1, z2 = merge_diff_grids(xmin, xmax, y1, y2)
-    
+
     # Difference
     plt.bar(
         e3[:-1], np.abs(z2-z1), np.diff(e3),
@@ -71,7 +71,7 @@ def plot_discrete_diff(xmin, xmax, y1, y2):
         fc=(0,0,0,0), ec='C1', lw=2, label='y1'
     )
     plt.bar(
-        e2[:-1], y2, np.diff(e2), 
+        e2[:-1], y2, np.diff(e2),
         align='edge',
         fc=(0,0,0,0), ec='C2', lw=2, label='y2'
     )
@@ -85,7 +85,7 @@ def plot_discrete_diff(xmin, xmax, y1, y2):
     # plt.plot(x3, z2, 'C4o-', label='z2')
 
     plt.legend()
-    
+
 def get_bin_centers(xmin, xmax, n1, n2):
     """n1, n2 are number of bins (n1+1, n2+1 edges)"""
     # Edges
@@ -95,7 +95,7 @@ def get_bin_centers(xmin, xmax, n1, n2):
     # Bin centers
     x1 = e1[:-1] + np.diff(e1)/2
     x2 = e2[:-1] + np.diff(e2)/2
-    
+
     return x1, x2
 
 def discrete_err(xmin, xmax, y1, y2, verbose=False, net=False):
@@ -134,30 +134,47 @@ def merge_linear(x1, x2, y1, y2):
     Linearly interpolate y1 and y2 onto
     the union of x1 and x2.
     """
-    
+
     # Use interp1d instead of griddata to enable 'extrapolate'
     x3 = np.sort([*x1, *x2])
     f1 = np.vectorize(interp1d(x1, y1, kind='linear', fill_value='extrapolate'))
     f2 = np.vectorize(interp1d(x2, y2, kind='linear', fill_value='extrapolate'))
     z1 = f1(x3)
     z2 = f2(x3)
-    return x3, z1, z2 
+    return x3, z1, z2
 
 def err_linear(xmin, xmax, x1, x2, y1, y2, net=False):
     """
     If net=True, calculate net area between curves.
     Otherwise, use absolute area between curves.
+
+    absolute error: average difference between curves
+    relative error: average difference between curves
+                    divided by average height of first curve
+
     """
     x3, z1, z2 = merge_linear(x1, x2, y1, y2)
+
     diff = z1-z2 if net else np.abs(z1-z2)
-    tot_abs_err = abs(np.trapz(
+    avg_abs_err = abs(np.trapz(
         x=x3,
         y=diff
-    ))
-    tot_rel_err = abs(tot_abs_err / np.trapz(x=x3, y=z1))
-    avg_abs_err, avg_rel_err = map(
-        lambda tot_err: tot_err / (xmax - xmin),
-        (tot_abs_err, tot_rel_err)
-    )
-    return avg_abs_err, avg_rel_err
+    ) / (xmax - xmin))
 
+    # Global version:
+    avg_rel_err = abs(np.trapz(
+        x=x3,
+        y=diff
+    ) / np.trapz(
+        x=x1,
+        y=y1
+    ))
+
+    # # Pointwise version:
+    # avg_rel_err = abs(np.trapz(
+    #     x=x3,
+    #     y=(diff / z1)
+    # ))
+
+
+    return avg_abs_err, avg_rel_err
